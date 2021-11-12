eacaf659a08aa36fee9b3d04b452e59bd4a4fed1
metazenic <ncristaldo3597@gmail.com>
2021-11-12 03:55:57 -0300

    [ADD] variables.css + estilo al header

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -2,10 +2,11 @@ import Style from "./Style.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={Style.cabezal}>
       <h1 className={Style.titulo}>
         <a href="#">Vorágine Voráz</a>
       </h1>
+      <p className={Style.subtitulo}>Escritura y coso</p>
     </header>
   );
 };
