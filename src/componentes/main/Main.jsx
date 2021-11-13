b5d8d43a29c1a7b24582ebf8bd2bfb85cd6408b5
metazenic <ncristaldo3597@gmail.com>
2021-11-13 12:25:46 -0300

    [MOD] comentarios sobre Main

--- src/componentes/main/Main.jsx
+++ src/componentes/main/Main.jsx
@@ -3,6 +3,7 @@ import Style from "./Style.module.css";
 const Main = () => {
   return (
     <main className={Style.cuerpo}>
+      {/* hardcodeado para ver qué onda */}
       <p className={Style.placeholder}>Main</p>
       <p className={Style.placeholder}>Main</p>
       <p className={Style.placeholder}>Main</p>
