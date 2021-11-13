7202ffbf5912d65192d7d26fb0d194f300709764
metazenic <ncristaldo3597@gmail.com>
2021-11-13 12:24:33 -0300

    [ADD] Main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./componentes/header/Header";
 import Footer from "./componentes/footer/Footer";
+import Main from "./componentes/main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/componentes/main/Main.jsx
@@ -0,0 +1,13 @@
+import Style from "./Style.module.css";
+
+const Main = () => {
+  return (
+    <main className={Style.cuerpo}>
+      <p className={Style.placeholder}>Main</p>
+      <p className={Style.placeholder}>Main</p>
+      <p className={Style.placeholder}>Main</p>
+    </main>
+  );
+};
+
+export default Main;
